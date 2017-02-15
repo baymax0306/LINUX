03a5192c160f57840068abf01f64baaf2b2f591b
zhongweichang <15979771469@163.com>
2017-02-15 11:41:26 +0800

    shell commit

---
+++ parameter.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "File Name: $0"
+echo "First Parameter: $1"
+echo "Second Parameter: $2"
+echo "Quoted Values: $@"
+echo "Quoted VAlues: $*"
+echo "Total Number of Parameter: $#"
+
---
+++ read.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+echo "What is your name?"
+read NAME
+echo "Hello, $NAME"
