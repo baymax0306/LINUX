ba0bb4ac814612f266a1229c7dda73b9fdce8bc8
zhongweichang <15979771469@163.com>
2017-02-15 15:10:20 +0800

    rm *.sh

--- parameter.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "File Name: $0"
-echo "First Parameter: $1"
-echo "Second Parameter: $2"
-echo "Quoted Values: $@"
-echo "Quoted VAlues: $*"
-echo "Total Number of Parameter: $#"
-
